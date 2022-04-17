8a416ef621ca4efff775fce82f6d69dbc707c9a0
Haluk D <76127756+halukdundar@users.noreply.github.com>
2022-04-17 18:27:44 +0300

    Add files via upload

---
+++ PozitifNegatifSayi.py
@@ -0,0 +1,13 @@
+#GİRİLEN SAYININ POZİTİF Mİ NEGATİF Mİ OLUP OLMADIĞI
+
+sayi = input('Bir Sayı Giriniz : ')
+ 
+if(int(sayi)>0):
+     print(sayi +" Sayısı Pozitif Sayıdır.")
+ 
+if(int(sayi)<0):
+     print(sayi +" Sayısı Negatif Sayıdır.")
+ 
+if(int(sayi)==0):
+     print(sayi +" Sayısı Sıfırdır.")
+ 
